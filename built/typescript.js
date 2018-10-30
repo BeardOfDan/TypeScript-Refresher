c1b11913b0dc173d8606789147180aeeb55dd835
Daniel Roberts <BeardOfDan@gmail.com>
2018-10-29 21:10:43 -0700

    (feat) Run through different variable types

--- built/typescript.js
+++ built/typescript.js
@@ -1,5 +1,11 @@
-const sum = (a, b) => {
-    return a + b;
+let isCool = true;
+let age = 42;
+let eyeColor = 'brown';
+let favoriateQuote = `I'm not old, I'm only ${age}`;
+let pets = ['cat', 'dog', 'pig'];
+let pets2 = ['lion', 'tiger', 'bear'];
+let wizard = {
+    a: 'John'
 };
-const answer = sum(4, 5);
-console.log(answer);
+let meh = undefined;
+let unbeing = null;
--- src/typescript.ts
+++ src/typescript.ts
@@ -1,7 +1,16 @@
-const sum = (a: number, b: number) => {
-  return a + b;
+let isCool: boolean = true;
+
+let age: number = 42;
+
+let eyeColor: string = 'brown';
+let favoriateQuote: string = `I'm not old, I'm only ${age}`;
+
+let pets: string[] = ['cat', 'dog', 'pig'];
+let pets2: Array<string> = ['lion', 'tiger', 'bear'];
+
+let wizard: object = {
+  a: 'John'
 };
 
-const answer = sum(4, 5);
-
-console.log(answer);
+let meh: undefined = undefined;
+let unbeing: null = null;
